e795b0a8bfd7957dc38dfd0fa7d6ba5217a07b52
carryzhang123 <1135484406@qq.com>
2017-08-26 20:17:48 +0800

    job loggint

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * @author ZhangHang
@@ -8,39 +6,7 @@ import java.util.Comparator;
  **/
 public class test {
     public static void main(String[] args) {
-        Student student=new Student(4);
-        Student student2=new Student(41);
-        Student student3=new Student(2);
-        ArrayList<Student> list=new ArrayList<Student>();
-        list.add(student);
-        list.add(student2);
-        list.add(student3);
-        Collections.sort(list, new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return o2.getId()-o1.getId();
-            }
-        });
-
-        for(Student student1:list){
-            System.out.println(student1.getId());
-        }
-    }
-}
-
-
-class Student{
-    int id;
-
-    public Student(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
+        HashMap<Integer,Long> map=new HashMap<Integer, Long>();
+        System.out.println(map.get(1));
     }
 }
