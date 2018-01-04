dc667ce1a0c1b70a5963cb88dde94896b51dbdbf
carryzhang123 <1135484406@qq.com>
2018-01-04 22:02:07 +0800

    job loggint

--- src/test/java/Father.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author ZhangHang
- * @create 2018-01-04 15:38
- **/
-public class Father {
-    public Father(int num){
-
-    }
-}
--- src/test/java/Son.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author ZhangHang
- * @create 2018-01-04 15:39
- **/
-public class Son extends Father {
-    public Son() {
-        super(1);
-    }
-}
