854fdf0b01b6375cd0f12fb665ac668caca3f4f2
carryzhang <1135484406@qq.com>
2018-08-14 00:53:38 +0800

    review

--- Ssm/src/main/java/com/javen/dao/IUserDao.java
+++ Ssm/src/main/java/com/javen/dao/IUserDao.java
@@ -1,24 +1,18 @@
 package com.javen.dao;
 
-import com.javen.mapper.UserMapper;
 import com.javen.model.User;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 
-/**
- * @author ZhangHang
- * @create 2018-08-08 1:15
- **/
-public class IUserDao {
-    @Autowired
-    private SqlSessionFactory sqlSessionFactory;
 
-    public User selectByPrimaryKey(int userId){
-        SqlSession sqlSession=sqlSessionFactory.openSession();
+public interface IUserDao {
+    int deleteByPrimaryKey(Integer id);
 
-        UserMapper userMapper=sqlSession.getMapper(UserMapper.class);
+    int insert(User record);
 
-        return userMapper.selectByPrimaryKey(userId);
-    }
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
 }
